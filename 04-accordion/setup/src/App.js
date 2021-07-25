4b4703903bc6f6e7c26ddcbeae23ad38f34da6a9
Imran Rather <emmeiwhite@gmail.com>
2021-07-25 13:53:14 +0530

    CSS completed.

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main className="mainWrapper">
+      <div className="questionAnswerHeading">
+        <h2>Questions and Answers About Login</h2>
+      </div>
+
+      <div className="mainQuestionsWrapper">
+        {questions.map((question) => {
+          return <SingleQuestion {...question} />;
+        })}
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,17 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const Question = ({ id, title, info }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <article className="questionWrapper">
+      <div className="mainTitle">
+        <h4>{title}</h4>
+        {isOpen ? <AiOutlineMinus /> : <AiOutlinePlus />}
+      </div>
+      {isOpen && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
